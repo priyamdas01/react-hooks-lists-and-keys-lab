5d09a6032f38a14b5b2c49d49aee4d721ba0c428
Priyam Das <domidas01@gmail.com>
2023-06-14 18:07:14 -0400

    complete lab

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,16 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
+  const jsxLinks = links.map((link)=> {
+    // let href = link;
+    return <a key = {link} href={"#"+link}>{link}</a>
+  }
+  )
+  // console.log(jsxLinks);
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>
+    {jsxLinks}
+  </nav>;
 }
 
 export default NavBar;
